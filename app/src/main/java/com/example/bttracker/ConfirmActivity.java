e1773691bf55244c7bb5f07e0183fb69ab550b1e
Yuuta Fujii <2020m077@kuas.ac.jp>
2020-06-18 21:03:40 +0900

    Added ConfirmActivity
    implemented data transfer using Intent in LogActivity

---
+++ app/src/main/java/com/example/bttracker/ConfirmActivity.java
@@ -0,0 +1,34 @@
+package com.example.bttracker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ConfirmActivity extends AppCompatActivity {
+    TextView accessData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm);
+        // find the ID of the TextView on the layout
+        accessData = (TextView)findViewById(R.id.showBTData);
+
+        // Extract the extras from Intent
+        Bundle transferredData = getIntent().getExtras();
+        // Extract data with the key "BT_data"
+        String s = transferredData.getString("BT_data");
+        // create the confirmation message
+        String txt = "Your input body temperature is " + s;
+        // show the confirmation message in the TextView
+        accessData.setText(txt);
+    }
+
+    public void backToHome(View view) {
+        Intent backHome = new Intent(this, MainActivity.class);
+        startActivity(backHome);
+    }
+}
--- app/src/main/java/com/example/bttracker/LogActivity.java
+++ app/src/main/java/com/example/bttracker/LogActivity.java
@@ -1,14 +1,31 @@
 package com.example.bttracker;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LogActivity extends AppCompatActivity {
 
+    EditText userInput;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
+        // get the ID of the EditText on the layout
+        userInput = (EditText)findViewById(R.id.LoginputDate);
+    }
+
+    public void confirmBT(View view) {
+        Intent toConfirm=new Intent(this, ConfirmActivity.class);
+        // append user input to the toConfirm Intent
+        // the 2nd argument means: get user input from the EditText View,
+        // and convert the text to a string
+        toConfirm.putExtra("BT_data", userInput.getText().toString());
+        // start ConfirmActivity
+        startActivity(toConfirm);
     }
 }
